9cfe1504847a1186fdc75f4c9a73e5ccfa1526b3
Pramilagupta <pramilafgupta1996@gmail.com>
2025-05-13 14:56:51 +0530

    test route addded

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import {v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -10,14 +10,13 @@ import notificationRoutes from "./routes/notification.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
-
 dotenv.config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -25,21 +24,25 @@ const PORT = process.env.PORT || 5000;
 // console.log(process.env.MONGO_URI);
 
 // middlewares
- app.use(express.json({limit: "5mb"})); // to parse req.body
+app.use(express.json({ limit: "5mb" })); // to parse req.body
 //  limit shouln't be too high to prevent DOS
- app.use(express.urlencoded({ extended: true })); //to parse from data(urlencoded)
+app.use(express.urlencoded({ extended: true })); //to parse from data(urlencoded)
 
- app.use(cookieParser());
+app.use(cookieParser());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/users",userRoutes);
-app.use("/api/posts",postRoutes);
-app.use("/api/notifications",notificationRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
+// test route
+app.get("/", (req, res) => {
+  res.send("Hello from backend server");
+});
 
 app.listen(PORT, () => {
-    console.log(`Backend server is running on port ${PORT}`);
-    connectMongoDB();
+  console.log(`Backend server is running on port ${PORT}`);
+  connectMongoDB();
 });
 
 export default app;
