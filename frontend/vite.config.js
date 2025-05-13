46096bec89794904552d1feb8c20f5b238457e97
Pramilagupta <pramilafgupta1996@gmail.com>
2025-05-13 15:07:10 +0530

    prod server url added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy:{
       "/api":{
-        target: "http://localhost:5000",
+        target: "https://fast-temple-34808-1abd3fc375e5.herokuapp.com/", // for dev server http://localhost:5000/
         changeOrigin: true,
       }
     }
